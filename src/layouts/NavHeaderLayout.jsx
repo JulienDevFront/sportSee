49177550a325855cb07c0f5eccea79575acd1e2c
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-01 11:48:35 +0200

    chore(rename): rename NavHeader to NavHeaderLayout

--- src/layouts/NavHeaderLayout.jsx
+++ src/layouts/NavHeaderLayout.jsx
@@ -1,14 +1,14 @@
-import styles from "../assets/styles/layouts/navHeader.module.scss";
+import styles from "../assets/styles/layouts/navHeaderLayout.module.scss";
 import logo from "../assets/icons/iconSportSee.svg";
 import ButtonLogo from "../components/ButtonLogo.jsx";
 import ButtonLink from "../components/ButtonLink.jsx";
 
-export default function NavHeader() {
+export default function NavHeaderLayout() {
 
     const routes = ["Accueil", "Profil", "Réglage", "Communauté"];
 
     return (
-        <nav className={ styles.navHeader}>
+        <nav className={ styles.navHeaderLayout}>
             <ButtonLogo logo={ logo } alt="SportSee logo" text="SportSee"/>
             { routes.map((item, index) => <ButtonLink key={ index } text={ item } />) }
         </nav>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import styles from "../assets/styles/pages/homePage.module.scss";
-import NavHeader from "../layouts/NavHeader.jsx";
+import NavHeaderLayout from "../layouts/NavHeaderLayout.jsx";
 
 export default function HomePage() {
 
     return (
         <div className={ styles.homePage } >
-        <NavHeader />    
+        <NavHeaderLayout />    
             Hello World
         </div>
     );
