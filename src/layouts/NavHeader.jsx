1730aa77a2801ee6fb58f25da6c334ea88cc6b50
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-01 10:42:27 +0200

    feat(layout): add NavHeader and style test

---
+++ src/layouts/NavHeader.jsx
@@ -0,0 +1,8 @@
+import styles from "../assets/styles/layouts/navHeader.module.scss";
+
+export default function NavHeader() {
+
+    return (
+        <nav className={ styles.navHeader}></nav>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,12 @@
 import styles from "../assets/styles/pages/homePage.module.scss";
+import NavHeader from "../layouts/NavHeader.jsx";
 
 export default function HomePage() {
 
     return (
-        <div className={ styles.homePage } >Hello World</div>
+        <div className={ styles.homePage } >
+        <NavHeader />    
+            Hello World
+        </div>
     );
 };
