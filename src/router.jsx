9fb17e9bf155e1b138bf1eeb69cfc775cb2f9624
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-08 12:21:19 +0200

    feat(callRootService): add callRootService for select context call root

--- src/router.jsx
+++ src/router.jsx
@@ -2,8 +2,6 @@ import "./assets/styles/index.css";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 // - - -
-import { getMainData, getUserAverageSession, getUserActivity, getUserPerformance } from "./services/getRouteData.js";
-// - - -
 import AppContentPage from "./pages/AppContentPage.jsx";
 import HomePage from "./pages/HomePage.jsx";
 
@@ -14,8 +12,3 @@ createRoot(document.getElementById('root')).render(
     </AppContentPage>
   </StrictMode>
 );
-
-getMainData(12, '/__mocks__/USER_MAIN_DATA.json').then((data) => console.log("USER_MAIN_DATA :", data));
-getUserAverageSession(12, '/__mocks__/USER_AVERAGE_SESSIONS.json').then((data) => console.log("USER_AVERAGE_SESSIONS :", data));
-getUserActivity(12, '/__mocks__/USER_ACTIVITY.json').then((data) => console.log("USER_ACTIVITY :", data));
-getUserPerformance(12, '/__mocks__/USER_PERFORMANCE.json').then((data) => console.log("USER_PERFORMANCE :", data));
