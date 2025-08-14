2b170ef6feef06a50858d917e3f07647fbda2302
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-14 22:36:54 +0200

    chore(review): review fetchJson service

--- src/services/fetchJson.js
+++ src/services/fetchJson.js
@@ -2,6 +2,7 @@ export const fetchJson = async (route) => {
     try {
         const res = await fetch(route);
         if(res.ok) console.log(`Call from "${route}" status : ${res.status}`);
+        if(!res.ok) return null;
         return await res.json();
     } catch(error) {
         console.error(`Call from "${route}" status : Error \n${error}`);
