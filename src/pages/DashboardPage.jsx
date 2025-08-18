2c125fde5fdd81c85cf18c865bcf2740bfd0fa44
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-08-18 18:46:33 +0200

    feat(view): create the chart of ChartPerformanceView and styles

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,7 @@
 import styles from "../assets/styles/pages/dashboardPage.module.scss";
 import { ApiContext } from "../context/ApiProvider.jsx";
 import { useContext, useState, useEffect } from "react";
-import { getMainData, getUserActivity, getUserAverageSession } from "../services/userData.js";
+import { getMainData, getUserActivity, getUserAverageSession, getUserPerformance } from "../services/userData.js";
 import { useParams } from "react-router";
 
 import ButtonToggleMode from "../components/ButtonToogleMode.jsx";
@@ -9,6 +9,7 @@ import HeaderWelcomeView from "../views/HeaderWelcomeView.jsx";
 import AsideNutritionView from "../views/AsideNutritionView.jsx";
 import ChartActivityView from "../views/ChartActivityView.jsx";
 import ChartAverageSessionsView from "../views/ChartAverageSessionsView.jsx";
+import ChartPerformanceView from "../views/ChatPerformanceView.jsx";
 
 export default function DashboardPage() {
     const { mode } = useContext(ApiContext);
@@ -16,12 +17,14 @@ export default function DashboardPage() {
     const [mainData, setMainData] = useState(null);
     const [activityData, setActivityData] = useState(null);
     const [sessionsData, setSessionsData] = useState(null);
+    const [performanceData, setPerformanceData] = useState(null);
 
     useEffect(() => {
         if (!mode?.baseURL || !userId) return;
         getMainData(mode, userId).then(setMainData);
         getUserActivity(mode, userId).then(setActivityData);
         getUserAverageSession(mode, userId).then(setSessionsData);
+        getUserPerformance(mode, userId).then(setPerformanceData);
     }, [mode, userId]);
 
     return (
@@ -35,6 +38,7 @@ export default function DashboardPage() {
                                 <ChartActivityView activityData={ activityData }/>
                                 <div>
                                     <ChartAverageSessionsView sessionsData={ sessionsData }/>
+                                    <ChartPerformanceView performanceData={ performanceData }/>
                                 </div>
                             </div>
                             <AsideNutritionView unit={ mainData.keyData }/>
