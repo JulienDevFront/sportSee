3391c77cc0f9f7ca3a1ecbdce34e75de09b841ca
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-09-11 17:54:16 +0200

    fix(view): fix and refactor HeaderNavView view

--- src/views/HeaderNavView.jsx
+++ src/views/HeaderNavView.jsx
@@ -4,17 +4,20 @@ import ButtonLogo from "../components/ButtonLogo.jsx";
 import ButtonLink from "../components/ButtonLink.jsx";
 
 export default function HeaderNavView() {
-
-    const routes = ["Accueil", "Profil", "Réglage", "Communauté"];
-
-    return (
-        <header className={ styles.headerNavView}>
-            <ButtonLogo path="#" img={ iconSportSee } alt="Back Home !" text="SportSee"/>
-            <nav>
-                { routes.map((item, index) => 
-                    <ButtonLink key={ index } path="#" text={ item } />) 
-                }
-            </nav>
-        </header>
-    );
-};
+	const routes = ["Accueil", "Profil", "Réglage", "Communauté"];
+	return (
+		<header className={styles.headerNavView}>
+			<ButtonLogo
+				path="#"
+				img={iconSportSee}
+				alt="Back Home !"
+				text="SportSee"
+			/>
+			<nav>
+				{routes.map((item, index) => (
+					<ButtonLink key={index} path="#" text={item} />
+				))}
+			</nav>
+		</header>
+	);
+}
