0af8dcd6ce8ff215a403484e27211b44608f32f7
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-09-11 17:52:50 +0200

    fix(view): fix and refactor the AsideNutritionView view

--- src/views/AsideNutritionView.jsx
+++ src/views/AsideNutritionView.jsx
@@ -1,18 +1,37 @@
 import styles from "../assets/styles/views/asideNutritionView.module.scss";
-import NutritionLegend from "../components/NutritionLegend.jsx"
-import iconCalories from "../assets/icons/iconCalories.svg"
-import iconCarbs from "../assets/icons/iconCarbs.svg"
-import iconFat from "../assets/icons/iconFat.svg"
-import iconProtein from "../assets/icons/iconProtein.svg"
+import NutritionLegend from "../components/NutritionLegend.jsx";
+import iconCalories from "../assets/icons/iconCalories.svg";
+import iconCarbs from "../assets/icons/iconCarbs.svg";
+import iconFat from "../assets/icons/iconFat.svg";
+import iconProtein from "../assets/icons/iconProtein.svg";
 
 export default function AsideNutritionView({ unit }) {
-
-    return (
-        <aside className={ styles.asideNutritionView }>
-            <NutritionLegend img={ iconCalories } alt="Stats" unit={ unit.calorieCount } legend="Calories"/>
-            <NutritionLegend img={ iconProtein } alt="Stats" unit={ unit.proteinCount } legend="Proteines"/>
-            <NutritionLegend img={ iconCarbs } alt="Stats" unit={ unit.carbohydrateCount } legend="Glucides"/>
-            <NutritionLegend img={ iconFat } alt="Stats" unit={ unit.lipidCount } legend="Lipides"/>
-        </aside>
-    );
-};
+	return (
+		<aside className={styles.asideNutritionView}>
+			<NutritionLegend
+				img={iconCalories}
+				alt="Stats"
+				unit={unit.calorieCount}
+				legend="Calories"
+			/>
+			<NutritionLegend
+				img={iconProtein}
+				alt="Stats"
+				unit={unit.proteinCount}
+				legend="Proteines"
+			/>
+			<NutritionLegend
+				img={iconCarbs}
+				alt="Stats"
+				unit={unit.carbohydrateCount}
+				legend="Glucides"
+			/>
+			<NutritionLegend
+				img={iconFat}
+				alt="Stats"
+				unit={unit.lipidCount}
+				legend="Lipides"
+			/>
+		</aside>
+	);
+}
