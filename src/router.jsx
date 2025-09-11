72cd9cff4d6ba4247603c1aa6d06011bb81fcc1f
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-09-11 19:04:40 +0200

    fix(router): fix and refactor the router

--- src/router.jsx
+++ src/router.jsx
@@ -1,24 +1,24 @@
 import "./assets/styles/index.css";
-import { StrictMode } from 'react';
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 // - - -
-import AppContent from "./app/AppContent.jsx";
+import AppPage from "./pages/AppPage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
 import { ApiProvider } from "./context/ApiProvider.jsx";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <StrictMode>
-      <ApiProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/user/" element={ <AppContent /> }>
-              <Route path=":userId" element={ <DashboardPage /> }/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ApiProvider>
-  </StrictMode>
+	<StrictMode>
+		<ApiProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/user/" element={<AppPage />}>
+						<Route path=":userId" element={<DashboardPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ApiProvider>
+	</StrictMode>
 );
