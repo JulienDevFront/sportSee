49b9f6f920864e1c759111ab3ab6ebf7a1044328
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-09-11 19:03:50 +0200

    fix(provider): fix and refactor ApiProvider

--- src/context/ApiProvider.jsx
+++ src/context/ApiProvider.jsx
@@ -3,25 +3,26 @@ import { createContext, useState } from "react";
 export const ApiContext = createContext();
 
 export const DATA_MODE = [
-    { name: "HTTP", baseURL: "http://localhost:3000"}, 
-    { name: "MOCK", baseURL: "http://localhost:5173"},
+	{ name: "HTTP", baseURL: "http://localhost:3000" },
+	{ name: "MOCK", baseURL: "http://localhost:5173" },
 ];
 
 export const ApiProvider = ({ children }) => {
-    const [mode, setMode] = useState(DATA_MODE.find( item => item.name === "MOCK"));
-    
-    const handleMode = (e) => {
-        setMode( mode => mode.name === "MOCK" 
-            ? DATA_MODE.find(i => i.name === "HTTP")
-            : DATA_MODE.find(i => i.name === "MOCK")
-        )
-    }
-    
-    console.log("Load App with mode :", mode);
+	const [mode, setMode] = useState(
+		DATA_MODE.find((item) => item.name === "MOCK")
+	);
 
-    return (
-        <ApiContext.Provider value={{ mode, handleMode }}>
-            { children }
-        </ApiContext.Provider>
-    )
+	const handleMode = (e) => {
+		setMode((mode) =>
+			mode.name === "MOCK"
+				? DATA_MODE.find((i) => i.name === "HTTP")
+				: DATA_MODE.find((i) => i.name === "MOCK")
+		);
+	};
+
+	return (
+		<ApiContext.Provider value={{ mode, handleMode }}>
+			{children}
+		</ApiContext.Provider>
+	);
 };
